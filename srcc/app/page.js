fcceb0152d6ea819d039b4bc9da5f92ad9783406
devalto <51037255+dev-alto@users.noreply.github.com>
2023-10-29 09:33:53 -0700

    Add files via upload

---
+++ srcc/app/page.js
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+async function onFileUpload(formData) {
+  "use server"
+  const file = formData.get("file")
+  console.log(file.name.endsWith('.png'))
+  if (!file.name.endsWith('.png')) {
+    console.log("File is not a png!")
+    return "File is not a png!"
+  }
+  console.log("File name:", file.name, "size:", file.size)
+  return ("File name:", file.name, "size:", file.size)
+}
+
+export default function Main() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          CalHacks 2023 Image Pattern Recognition Demo
+          {/* <code className="font-mono font-bold">src/app/page.js</code> */}
+        </p>
+
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:border-neutral-800 dark:hg lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hacked by {'The Beauties of Joseon'}
+          </a>
+        </div>
+      </div>
+
+
+      <form action={onFileUpload}>
+        <label htmlFor="file"></label>
+        <input type="file" name="file" id="file"/>
+        <button type="submit" id="upload">Upload Image</button>
+0     </form>
+    </main>
+  )
+}
